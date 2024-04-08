d86b85e46a2a5a47bc40fddeb2e5698ee8cec672
Pham Qui Duong <phamquiduong@outlook.com>
2024-04-08 22:06:51 +0700

    Add group service doc string

--- src/services/group.py
+++ src/services/group.py
@@ -4,14 +4,41 @@ from models import Group as GroupModel
 
 
 def get_group(db: Session, group_id: int):
+    """Get group by id
+
+    Args:
+        db (Session): Database session
+        group_id (int): Group id
+
+    Returns:
+        GroupModel: Group By Id provided
+    """
     return db.query(GroupModel).filter(GroupModel.id == group_id).first()
 
 
 def get_group_by_name(db: Session, name: str):
+    """Get group by name
+
+    Args:
+        db (Session): Database session
+        name (str): Group name
+
+    Returns:
+        GroupModel: Group by Name provided
+    """
     return db.query(GroupModel).filter(GroupModel.name == name).first()
 
 
 def create_group(db: Session, name: str):
+    """Create a new group by name
+
+    Args:
+        db (Session): Database session
+        name (str): Group name
+
+    Returns:
+        GroupModel: Group created
+    """
     group_db = GroupModel(name=name)
     db.add(group_db)
     db.commit()
@@ -20,6 +47,15 @@ def create_group(db: Session, name: str):
 
 
 def get_or_create_group(db: Session, name: str):
+    """Get or create group if group with name does not exist
+
+    Args:
+        db (Session): Database session
+        name (str): Group name
+
+    Returns:
+        GroupModel: Group model
+    """
     group = get_group_by_name(db, name)
     if group is None:
         group = create_group(db, name)
