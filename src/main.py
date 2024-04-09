101ea199a4b5956c0f1e891bf0518582b600b0dd
Pham Qui Duong <phamquiduong@outlook.com>
2024-04-10 01:15:08 +0700

    Request Validation Error Custom

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.exceptions import RequestValidationError
 
 from config import APP_TITLE, APP_VERSION
 from exceptions import APIException
@@ -23,3 +24,21 @@ app.include_router(users_router)
 @app.exception_handler(APIException)
 async def fastapi_exception_handler(_, exc: APIException):
     return exc.get_response()
+
+
+@app.exception_handler(RequestValidationError)
+async def request_validation_error_handler(_, exc: RequestValidationError):
+    error_fields = {}
+    for pydantic_error in exc.errors():
+        loc, msg = pydantic_error["loc"], pydantic_error["msg"]
+        loc = loc[1:] if len(loc) > 1 and loc[0] in ("body", "query", "path") else ['__all__']
+        for field in loc:
+            if not isinstance(field, str):
+                field = '__all__'
+            error_fields[field] = msg
+    return APIException(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        error_code='ERR-422-000',
+        message='Request validation error',
+        error_fields=error_fields
+    ).get_response()
