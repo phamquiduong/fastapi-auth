6a5c51c062372e5079345e5bcc59d5839267c8f9
Pham Qui Duong <phamquiduong@outlook.com>
2024-04-10 00:50:50 +0700

    Add command copy config file when config file does not exists

--- run.bat
+++ run.bat
@@ -2,17 +2,31 @@
 REM Install all python packages
 pip install -r requirements.txt
 
+
 REM Run all migration tasks
 alembic upgrade head
+
 
 REM Change directory to the "src" folder
 cd /d "src"
 
+
+REM Copy config file when config file does not exist
+if not exist config.py (
+    copy config.example.py config.py
+    echo config.py copied successfully.
+) else (
+    echo config.py already exists.
+)
+
+
 REM Read the port number from the user (with default value 80)
 set /p PORT=Enter the port number (default is 80):
 
+
 REM Check if PORT variable is empty (default value)
 if "%PORT%"=="" set PORT=80
+
 
 REM Run the FastAPI app using uvicorn, checking for PORT value
 uvicorn main:app --host 0.0.0.0 --port %PORT% --reload
