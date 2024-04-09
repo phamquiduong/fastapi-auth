58aa235f144dfef804480df5886c54f4e7e57579
Pham Qui Duong <phamquiduong@outlook.com>
2024-04-10 01:28:01 +0700

    Set error_code APIException is required

--- src/schemas/error.py
+++ src/schemas/error.py
@@ -8,6 +8,6 @@ class FieldErrorSchema(BaseModel):
 
 class ErrorSchema(BaseModel):
     status_code: int
-    error_code: str | None = None
+    error_code: str
     message: str | None = None
     error_fields: list[FieldErrorSchema] | None = None
